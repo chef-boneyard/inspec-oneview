3bda27a8997871bfe97f8c4fae90f3d9d6d7d8fc
Partner_Access 1 <PartnerAccess_1@tme.lab>
2018-02-26 08:00:14 -0600

    Removed unncessary (for now) FilterTable construction

--- libraries/oneview_server_profile.rb
+++ libraries/oneview_server_profile.rb
@@ -7,15 +7,7 @@ class OneviewServerProfile < OneviewResourceBase
   desc 'InSpec resource to test aspects of a Server Profile'
 
   attr_accessor :probes
-
-  # Define the filter table for the resource
-  filter = FilterTable.create
-  filter.add_accessor(:where)
-        .add_accessor(:entries)
-        .add(:exists?) { |x| !x.entries.empty? }
-
-  filter.connect(self, :probes) 
-  
+ 
   # Constructor for the resource. This calls the parent constructor
   # to get the generic resource for the all the servers in OneView.
   #
@@ -52,20 +44,6 @@ class OneviewServerProfile < OneviewResourceBase
 
   def respond_to_missing?(*)
     true
-  end
-
-  def parse_resource(resource)
-    # Create a hash to hold the parsed data
-    parsed = {}
-
-    # iterate around the keys and values of the resource to build up the 
-    # parsed data
-    resource.each do |key, value|
-      parsed[snake_case(key)] = value
-    end
-
-    # return the pased hash to the calling function
-    parsed
   end
 
   def has_managed_bios?
